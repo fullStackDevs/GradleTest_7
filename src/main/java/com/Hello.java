00899238e76fd325b1d130caf156c50482d6f0a3
Petru Birzu <pbirzu@endava.net>
2016-11-14 18:14:48 +0200

    Initial commit

---
+++ src/main/java/com/Hello.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created by pbirzu on 11/14/2016.
+ */
+public class Hello {
+    public static void main(String[] arga){
+        System.out.println("Hello - JCL");
+
+    }
+}
